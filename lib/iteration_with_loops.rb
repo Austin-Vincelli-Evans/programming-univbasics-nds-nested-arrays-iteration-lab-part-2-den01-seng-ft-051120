f59af2f5ff0b6e1b9441a0debe711aac409d2e71
Austin Evans <unity7591@gmail.com>
2020-04-28 14:21:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,7 @@ row_index=0
     element_index = 0 
     while element_index < src[row_index].count do
       src[row_index].sort 
-      daily_lows=src.shift
-      
+      daily_lows=src[row_index].shift
       element_index += 1
     end
   row_index += 1  
