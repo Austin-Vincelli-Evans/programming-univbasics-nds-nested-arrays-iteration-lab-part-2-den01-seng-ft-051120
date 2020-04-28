3c3901c6b7f4d67161dbc774e70d82eacabb5ff8
Austin Evans <unity7591@gmail.com>
2020-04-28 14:04:13 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,9 +3,9 @@ daily_lows=[]
 row_index=0 
   while row_index < src.count do
     element_index = 0 
-    while element_index < src[row_index].count do
-      if src[row_index][element_index] <= 20 
-        daily_lows = src[row_index][element_index]
+    while element_index <= src[row_index].count do
+      if src[row_index][element_index] <= src[row_index].min 
+        daily_lows << src[row_index][element_index]
       end
       element_index += 1
     end
